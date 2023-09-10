f8d47abb6b82d13a209fc953a48779ab4777805a
Ryan Carr <carr.ryanedward@gmail.com>
2023-09-10 09:43:42 -0400

    gallery update 5

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -25,7 +25,9 @@ const Gallery = () => {
       <div className='row justify-content-left mb-4'>
         <div className='col col-md-3 col-lg-3'>
           <div className='form-group'>
-            <label for='formControlSelect' className='mb-2'>Filter by category</label>
+            <label htmlFor='formControlSelect' className='mb-2'>
+              Filter by category
+            </label>
             <select
               className='form-select'
               id='formControlSelect'
@@ -55,117 +57,18 @@ const Gallery = () => {
       <div className='row g-4 py-3 row-cols-1 row-cols-md-2 row-cols-lg-3'>
         {artGallery.map((a) => (
           <Art
-            key={a.name}
+            key={a.uuid}
             name={a.name}
-            status={a.status}
+            sold={a.sold}
             desc={a.description}
             price={a.price}
             image={a.image}
             category={a.category}
+            notes={a.notes}
           />
         ))}
       </div>
     </div>
-    /*
-    <div className='container-fluid mt-3'>
-      <div className='row'>
-        <div className='col-lg-3 col-md-6 mb-4'>
-          <img src={art16} className='img-fluid mb-4' alt='' />
-          <img src={art02} className='img-fluid mb-4' alt='' />
-          <img
-            src={art01}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.3s'
-          />
-          <img
-            src={art09}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.5s'
-          />
-        </div>
-        <div className='col-lg-3 col-md-6 mb-4'>
-          <img
-            src={art03}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.1s'
-          />
-          <img
-            src={art05}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.4s'
-          />
-          <img
-            src={art07}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-          <img
-            src={art10}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-        </div>
-        <div className='col-lg-3 col-md-6 mb-4'>
-          <img
-            src={art06}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-          <img
-            src={art04}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.5s'
-          />
-          <img
-            src={art08}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-          <img
-            src={art11}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.5s'
-          />
-        </div>
-        <div className='col-lg-3 col-md-6 mb-4'>
-          <img
-            src={art14}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-          <img
-            src={art15}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.5s'
-          />
-          <img
-            src={art13}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.2s'
-          />
-          <img
-            src={art12}
-            className='img-fluid mb-4'
-            alt=''
-            data-wow-delay='0.5s'
-          />
-        </div>
-      </div>
-    </div>
-    */
   );
 };
 
