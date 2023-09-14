ef5d036dbfe03e4ac28437835f495f3302d03e39
Ryan Carr <carr.ryanedward@gmail.com>
2023-09-14 09:21:34 -0400

    updates

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import useAbout from '../../Hooks/useAbout';
 
 const About = () => {
   const [about, isLoading] = useAbout();
-
-  useEffect(() => {
-    console.log(about);
-  }, [about]);
 
   if (isLoading)
     return (
--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -23,16 +23,12 @@ const Gallery = () => {
 
   useEffect(() => {
     setArtGallery(sculptures);
-    console.log(sculptures);
   }, [sculptures]);
 
   if (isLoading)
     return (
       <div className='d-flex justify-content-center mx-5 my-5'>
-        <div
-          className='spinner-border text-dark'
-          role='status'
-        >
+        <div className='spinner-border text-dark' role='status'>
           <span className='visually-hidden'>Loading...</span>
         </div>
       </div>
